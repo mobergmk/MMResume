44ea963dba1f941b87777e8d91640c7bbffd40b4
mobergmk <mobergmk@gmail.com>
2017-06-04 06:58:31 -0500

    Link Added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -140,6 +140,7 @@ var project = {
         },
         {
             "title": "Project 2",
+            "hyperlink": "https://mobergmk.github.io/MMAbout"
             "dates": "2016",
             "description": "My Project 2",
             "images": ["images/thehelpdocs.png"]
